fe1db3df15251ef80dbc626fd2dde01ba2c84a48
Canh Trinh <canh@axelar.network>
2022-08-18 09:12:07 -0700

    chore: update addLocalXc20 script

--- scripts/addLocalXc20.js
+++ scripts/addLocalXc20.js
@@ -1,6 +1,9 @@
 'use strict';
 require('dotenv').config();
-const { setJSON } = require('@axelar-network/axelar-local-dev/dist/utils');
+const {
+    utils: { setJSON },
+    testnetInfo,
+} = require('@axelar-network/axelar-local-dev');
 const { Wallet } = require('ethers');
 const { keccak256, defaultAbiCoder } = require('ethers/lib/utils');
 const index = require(`./index.js`);
@@ -16,9 +19,24 @@ module.exports = {
 if (require.main === module) {
     const wallet = new Wallet(process.env.PRIVATE_KEY);
 
-    const chains = require(`../info/testnet.json`);
+    const env = process.argv[2];
+    if (env === null || (env !== 'testnet' && env !== 'local'))
+        throw new Error('Need to specify tesntet or local as an argument to this script.');
+    let temp;
+
+    if (env === 'local') {
+        temp = require(`../info/local.json`);
+    } else {
+        try {
+            temp = require(`../info/testnet.json`);
+        } catch {
+            temp = testnetInfo;
+        }
+    }
+
+    const chains = temp;
 
     addLocalXc20(chains[0], wallet).then(() => {
-        setJSON(chains, './info/testnet.json');
+        setJSON(chains, `./info/${env}.json`);
     });
 }
